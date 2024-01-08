bb682fa7f801191152ae93e323e95327a464e5e6
demigod66 <fifirman000@gmail.com>
2024-01-08 09:13:13 +0700

    make a Order post with some error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
 
@@ -30,10 +32,13 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
-function Header(props) {
+function Header() {
   return (
     <header>
       <Link to="/">Pizza Hut</Link>
+      <SearchOrder />
       <p>Firman</p>
     </header>
   );
